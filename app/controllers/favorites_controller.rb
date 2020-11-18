bcdda0b1db1e8370576b16508e7f12ce4d5b6feb
EC2 Default User <ec2-user@ip-172-31-37-182.ec2.internal>
2020-11-18 08:57:07 +0000

    コメント機能を追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,9 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_back(fallback_location: books_path)
+    # redirect_back(fallback_location: books_path)
+    redirect_to request.referer
+
 
   end
 
@@ -11,6 +13,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_back(fallback_location: books_path)
+    # redirect_back(fallback_location: books_path)
+    redirect_to request.referer
   end
 end
